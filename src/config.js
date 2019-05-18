adeb35d35d9beebdd5b831e0c10caf0d724d1e9d
Kane Clover <kane@assure24.com>
2019-05-18 12:24:48 +0100

    remove config file with (now dead) API key. Oops

--- src/config.js
+++
@@ -1,6 +0,0 @@
-export const GOOGLE_API_KEY = "AIzaSyCYW9IFajLDOCE8VJ4aTujmilIcTsomCKk";
-export const DEFAULT_SEARCH_RADIUS = 5;
-
-export const darkGreen = "#19481c";
-export const lightGreen = "#37a33d";
-export const errorRed = "#dd0000";
