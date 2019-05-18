5a9e143baf6ec3d11d806774904b5eec1955cc04
Kane Clover <kane@assure24.com>
2019-05-18 12:38:08 +0100

    Example config + ignore actual one

---
+++ src/config.example.js
@@ -0,0 +1,6 @@
+export const GOOGLE_API_KEY = "API_KEY_HERE";
+export const DEFAULT_SEARCH_RADIUS = 5;
+
+export const darkGreen = "#19481c";
+export const lightGreen = "#37a33d";
+export const errorRed = "#dd0000";
